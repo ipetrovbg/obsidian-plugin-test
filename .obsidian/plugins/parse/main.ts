a9fde758d3d736393b1ea0d4065348744236c64d
petarpetrov <ipetrovbg@gmail.com>
2021-03-08 21:58:08 +0200

    sync

--- .obsidian/plugins/parse/main.ts
+++ .obsidian/plugins/parse/main.ts
@@ -8,13 +8,13 @@ export default class GitHubSyncPlugin extends Plugin {
     public async onload(): Promise<void> {
 
         const rootPath = normalizePath((this.app.vault.adapter as any).basePath);
-        const readyCmd = `cd '${rootPath}' && git pull`;
+        const readyCmd = `cd "${rootPath}" && git pull`;
         new Notice(this.gitSyncMessage);
         exec(readyCmd, this.handleGitCommand.bind(this));
 
         this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
 			if (evt.which === 83 && evt.metaKey || evt.ctrlKey && evt.which === 83) {
-                const command = `cd '${rootPath}' && git add . && git commit -m "sync" && git push`;
+                const command = `cd "${rootPath}" && git add . && git commit -m "sync" && git push`;
 
                 new Notice(this.gitSyncMessage);
 
