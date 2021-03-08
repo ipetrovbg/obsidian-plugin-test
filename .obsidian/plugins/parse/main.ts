d75cc046c1d5a13d351188d1318d3737dd619a8d
petarpetrov <ipetrovbg@gmail.com>
2021-03-08 20:41:06 +0200

    sync

--- .obsidian/plugins/parse/main.ts
+++ .obsidian/plugins/parse/main.ts
@@ -1,5 +1,5 @@
 import { normalizePath, Notice, Plugin, TFile, Workspace } from 'obsidian';
-import { exec } from "child_process";
+import {exec, ExecException} from "child_process";
 import {GitHubSettingTab} from './settings-tab';
 
 
@@ -10,12 +10,7 @@ export default class GitHubSyncPlugin extends Plugin {
         const rootPath = normalizePath((this.app.vault.adapter as any).basePath);
         const readyCmd = `cd '${rootPath}' && git pull`;
         new Notice(this.gitSyncMessage);
-        exec(readyCmd, (err, stdout) => {
-            if (err) {
-                return;
-            }
-            new Notice(stdout);
-        });
+        exec(readyCmd, this.handleGitCommand.bind(this));
 
         this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
 			if (evt.which === 83 && evt.metaKey) {
@@ -23,18 +18,24 @@ export default class GitHubSyncPlugin extends Plugin {
 
                 new Notice(this.gitSyncMessage);
 
-                exec(command, (err, stdout, stdErr) => {
-                    if (err) {
-                        new Notice("Already up to date.");
-                    } else {
-                        new Notice("Done.");
-                    }
-                });
-                
+                exec(command, this.handleGitCommand.bind(this));
+
             }
 		});
 
         this.addSettingTab(new GitHubSettingTab(this.app, this));
 
+    }
+
+    private handleGitCommand(err: ExecException | null) {
+        if (new RegExp('No configured push destination').test(err?.message)) {
+            new Notice("You need to setup git repository.");
+            return;
+        }
+        if (err) {
+            new Notice("Already up to date.");
+        } else {
+            new Notice("Done.");
+        }
     }
 }
